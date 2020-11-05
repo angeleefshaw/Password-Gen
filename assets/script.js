d7ffbba68d11818121b8f43adb13e8d780ebeaf8
Angelee Shaw <angeleefshaw@gmail.com>
2020-11-05 17:13:33 -0500

    Debug
    Fixed script tag in html, reworked function in Javascript. Spelling errors

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,46 @@
-const symbols= ['!','@','#','&','%','^','&','*','+','?']
-// const numbers = [1,2,3,4,5,6,7,8,9]
-// const upperCase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-const finalArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var password = generatePassword();
 
-const isChecked = (variableName, firstArr, secondArr) => {
-  if (variableName.checked === true) {
-    for (let i=0; i<firstArr.length; i++) {
-      secondArr.push(firstArr[i]);
-    }
-  } else {
-    secondArr.push('');
-  }
-}
+const passwordDisplay = document.getElementById('password');
+const includeSymbolsElement = document.getElementById("specialCharacter");
+const generateBtn = document.getElementById("generate");
+const characterAmountNumber= document.getElementById('characterAmount')
 
-const updatedArr = isChecked(speicalCharacter, symbols, finalArr)
-console.log(updatedArr)
 
-const specialCharacter = document.querySelector("#specialCharacter");
-const generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-// Generate password function that takes final array and shuffles it
-const generatePassword =(finalArray.map( finalArr => {
-  Math.floor(math.random() * finalArr.length))
-}
-
-// pushes the generated password to the text area 
-generateBtn.addEventListener('click', ()=> {
-  passwordText.innerhtml= generatePassword.join('');
+generateBtn.addEventListener('click', e => {
+  e.preventDefault()
+  const characterAmount = Number(characterAmountNumber.value)
+  const includeSymbols = includeSymbolsElement.checked
+  console.log(characterAmount,includeSymbols)
+  const password = generatePassword(characterAmount, includeSymbols);
+  passwordDisplay.innerText = password;
 })
 
 
-// var characterAmountinput = document.querySelector("#characterAmount");
+
+const generatePassword = (characterAmount, includeSymbols) => {
+const symbols= ['!','@','#','&','%','^','&','*','+','?']
+const numbers = [1,2,3,4,5,6,7,8,9]
+const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const finalArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let pass = "";
+finalArr.push(...numbers);
+// finalArr.push(...upperCase);
+if(includeSymbols) finalArr.push(...symbols) 
+for (let i = 0; i<characterAmount; i++) {
+  let character = finalArr[Math.floor(Math.random() *  finalArr.length)]
+  //generatePassword.push(character);
+  pass += character;
+}
+return pass
+}
 
 
 
-// passwordText.value = password;
-
-
-
-
-// var lowerCase = ['abcdefghijklmnopqrstuvwx,'y','z']
 
 
 
 
 
-// var numOfCharacters = characterAmountinput.value
-// specialCharacters= specialCharacterinput.value
+
+
+
 
