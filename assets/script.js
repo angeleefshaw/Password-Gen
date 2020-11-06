e69101acb1531a0b1e86a9bbef735918e0c688b6
Angelee Shaw <angeleefshaw@gmail.com>
2020-11-06 18:42:20 -0500

    Debug
    Fixing errors

--- assets/script.js
+++ assets/script.js
@@ -22,8 +22,8 @@ const numbers = [1,2,3,4,5,6,7,8,9]
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const finalArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let pass = "";
-finalArr.push(...numbers);
-// finalArr.push(...upperCase);
+// if(includeNums) finalArr.push(...numbers);
+// if(includeUpper) finalArr.push(...upperCase);
 if(includeSymbols) finalArr.push(...symbols) 
 for (let i = 0; i<characterAmount; i++) {
   let character = finalArr[Math.floor(Math.random() *  finalArr.length)]
