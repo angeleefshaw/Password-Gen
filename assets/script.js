8d45f5d06e2d86b3ec7902dbda5528804a5bf925
Angelee Shaw <angeleefshaw@gmail.com>
2020-11-06 19:13:46 -0500

    Added inputs
    Added Uppercase and Number inputs. Set up event listeners and updated generatePassword()

--- assets/script.js
+++ assets/script.js
@@ -2,29 +2,32 @@
 const passwordDisplay = document.getElementById('password');
 const includeSymbolsElement = document.getElementById("specialCharacter");
 const generateBtn = document.getElementById("generate");
-const characterAmountNumber= document.getElementById('characterAmount')
-
+const characterAmountNumber= document.getElementById('characterAmount');
+const num = document.getElementById("number");
+const upper = document.getElementById("uppercase"); 
 
 generateBtn.addEventListener('click', e => {
   e.preventDefault()
   const characterAmount = Number(characterAmountNumber.value)
   const includeSymbols = includeSymbolsElement.checked
-  console.log(characterAmount,includeSymbols)
-  const password = generatePassword(characterAmount, includeSymbols);
+  const includeNums = num.checked
+  const includeUpper = upper.checked
+  console.log(characterAmount,includeSymbols, includeNums, includeUpper)
+  const password = generatePassword(characterAmount, includeSymbols, includeNums, includeUpper);
   passwordDisplay.innerText = password;
 })
 
 
 
-const generatePassword = (characterAmount, includeSymbols) => {
+const generatePassword = (characterAmount, includeSymbols, includeNums, includeUpper) => {
 const symbols= ['!','@','#','&','%','^','&','*','+','?']
 const numbers = [1,2,3,4,5,6,7,8,9]
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const finalArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let pass = "";
-// if(includeNums) finalArr.push(...numbers);
-// if(includeUpper) finalArr.push(...upperCase);
-if(includeSymbols) finalArr.push(...symbols) 
+if(includeNums) finalArr.push(...numbers);
+if(includeUpper) finalArr.push(...upperCase);
+if(includeSymbols) finalArr.push(...symbols); 
 for (let i = 0; i<characterAmount; i++) {
   let character = finalArr[Math.floor(Math.random() *  finalArr.length)]
   //generatePassword.push(character);
